10073b346c83755f9a3995f895e2acfd0709606d
Elliot Bobrow <elliotgreybobrow@gmail.com>
2022-12-05 20:47:02 -0800

    rust day 5 getting ugly

---
+++ src/day5/mod.rs
@@ -0,0 +1,103 @@
+use crate::util::read_lines;
+
+fn sample() -> Vec<String> {
+    r#"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"#
+        .split('\n')
+        .map(String::from)
+        .collect()
+}
+
+struct Stacks {
+    stacks: Vec<Vec<char>>,
+}
+
+impl Stacks {
+    fn from(lines: &[String]) -> Self {
+        let mut lines: Vec<_> = lines[..lines.len() - 1]
+            .iter()
+            .map(|line| {
+                let line_vec: Vec<_> = line.chars().collect();
+                line_vec
+                    .chunks(4)
+                    .map(|c| {
+                        c.iter()
+                            .filter(|x| ![' ', '[', ']'].contains(x))
+                            .next()
+                            .unwrap_or(&' ')
+                            .clone()
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect();
+        lines.reverse();
+        let mut stacks = vec![Vec::new(); lines[0].len()];
+        for i in 0..lines[0].len() {
+            for line in &lines {
+                if line[i] != ' ' {
+                    stacks[i].push(line[i].clone());
+                }
+            }
+        }
+        Stacks { stacks }
+    }
+
+    fn mv(&mut self, from: usize, to: usize) {
+        let item = self.stacks[from - 1].pop().unwrap();
+        self.stacks[to - 1].push(item);
+    }
+
+    fn mv_many(&mut self, n: usize, from: usize, to: usize) {
+        let from = &mut self.stacks[from - 1];
+        let mut items: Vec<_> = from.drain(from.len() - n..).collect();
+        self.stacks[to - 1].append(&mut items);
+    }
+
+    fn tops(&self) -> String {
+        self.stacks
+            .iter()
+            .map(|stack| stack.last().unwrap())
+            .collect()
+    }
+}
+
+pub fn solve1() -> String {
+    // let data = sample();
+    let data = read_lines("/home/elliotbobrow/Downloads/day5.txt");
+    let mut data = data.split(String::is_empty);
+    let mut stacks = Stacks::from(data.next().unwrap());
+    let instructions = data.next().unwrap();
+    for instruction in instructions {
+        let parts: Vec<_> = instruction
+            .split(' ')
+            .filter_map(|word| word.parse::<usize>().ok())
+            .collect();
+        for _ in 0..parts[0] {
+            stacks.mv(parts[1], parts[2]);
+        }
+    }
+    stacks.tops()
+}
+
+pub fn solve2() -> String {
+    // let data = sample();
+    let data = read_lines("/home/elliotbobrow/Downloads/day5.txt");
+    let mut data = data.split(String::is_empty);
+    let mut stacks = Stacks::from(data.next().unwrap());
+    let instructions = data.next().unwrap();
+    for instruction in instructions {
+        let parts: Vec<_> = instruction
+            .split(' ')
+            .filter_map(|word| word.parse::<usize>().ok())
+            .collect();
+        stacks.mv_many(parts[0], parts[1], parts[2]);
+    }
+    stacks.tops()
+}
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 mod util;
 
 fn main() {
-    println!("{}", day4::solve1());
-    println!("{}", day4::solve2());
+    println!("{}", day5::solve1());
+    println!("{}", day5::solve2());
 }
